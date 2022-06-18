4d4ae4da68f11ebdaff8cd60cc29a53e8f2ea68c
Victor Robert <rinmai2019@gmail.com>
2022-06-18 18:35:01 +0700

    up

---
+++ style.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: 20,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
